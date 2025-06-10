1d92f6400dd2809da0afb67271ff44af85294726
Pereira-234 <afonsoaquino52@gmail.com>
2025-06-10 15:54:13 -0300

    commit - boa parte do cap 7

--- loja/loja/views/ProdutoView.py
+++ loja/loja/views/ProdutoView.py
@@ -1,7 +1,16 @@
-from django.http import HttpResponse
+from django.shortcuts import render, redirect # Retire from django.http import HttpResponse
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context, status=200)
 
 def list_produto_view(request, id=None):
     produto = request.GET.get("produto")
@@ -11,11 +20,6 @@ def list_produto_view(request, id=None):
     fabricante = request.GET.get("fabricante")
     dias = request.GET.get("dias")
     produtos = Produto.objects.all()
-
-    if dias is not None:
-        now = timezone.now()
-        now = now - timedelta(days = int(dias))
-        produtos = produtos.filter(criado_em__gte=now)
     if produto is not None:
         produtos = produtos.filter(Produto=produto)
     if promocao is not None:
@@ -26,9 +30,53 @@ def list_produto_view(request, id=None):
         produtos = produtos.filter(categoria__Categoria=categoria)
     if fabricante is not None:
         produtos = produtos.filter(fabricante__Fabricante=fabricante)
+    if dias is not None:
+        now = timezone.now()
+        now = now - timedelta(days = int(dias))
+        produtos = produtos.filter(criado_em__gte=now)
     if id is not None:
         produtos = produtos.filter(Produto__contains=produto )
     print(produtos)
     if id is None:
         id = 0
-    return HttpResponse('<h1>Produto de id %s!</h1>' % id)
+    context = {'produtos': produtos}
+    return render(request, template_name='produto/produto.html', context=context, status=200)
+
+# adicione a função que trata o postback da interface de edição
+def edit_produto_postback(request, id=None):
+    # Processa o post back gerado pela action
+    if request.method == 'POST':
+        # Salva dados editados
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+            obj_produto.save()
+            print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
+
+def details_produto_view(request, id=None):
+    # Processa o evento GET gerado pela action
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = {'produto': produto}
+    return render(request, template_name='produto/produto-details.html', context=context, status=200)
